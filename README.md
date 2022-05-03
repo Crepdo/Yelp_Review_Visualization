4d154359b88666427b0a57c7e442a2050ef878b0
Yifan song <Eric_YF_Song@outlook.com>
2022-05-03 21:02:28 +0800

    init from hw2

---
+++ README.md
@@ -0,0 +1,19 @@
+﻿# Yifan Song's hw2
+If you use vscode, use Ctrl+Shift+P,
+task: run tasks > task: install dependencies > task: run task: start app
+
+if not in vscode
+run npm install in both frontend and backend.
+Then run npm run start in both frontend and backend.
+```ps1
+cd backend
+npm install
+npm run start
+```
+```ps1
+cd frontend
+npm install
+npm run start
+```
+
+precompute python script is in backend/precompute folder.
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,47 @@
+﻿<template>
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="row justify-content-around">
+        <div class="col-8 col-md-5">
+          <h5 class="title">ARTS_hw2</h5>
+          <p class="description">All charts here.</p>
+        </div>
+        <div class="col-2">
+          <ul class="list-unstyled">
+            <li>
+              <a class="footerlink" href="/">Chart</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+
+<style scoped>
+.footer {
+  background-color: #1d1d1d;
+  padding-top: 2rem;
+  padding-bottom: 4rem;
+}
+
+.title {
+  color: #fff;
+}
+
+.description {
+  color: #fff;
+}
+
+.footerlink,
+.footerlink:hover {
+  color: #fff;
+}
+</style>
