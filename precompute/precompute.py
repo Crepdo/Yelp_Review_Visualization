ba8f00dbb6531538c2714df0f666480b085ad5f1
Yifan Song <Eric_YF_Song@outlook.com>
2022-05-12 17:55:24 +0800

    Added example comment and requirements.txt

--- precompute/precompute.py
+++ precompute/precompute.py
@@ -1,5 +1,14 @@
+def example_function(a: int) -> int:
+    '''
+    An example function.
+    :param a: An integer.
+    :returns: The square of a.
+    '''
+    return a * a
+
+
 def main():
-    pass
+    example_function(2)
 
 if __name__ == "__main__":
     main()
