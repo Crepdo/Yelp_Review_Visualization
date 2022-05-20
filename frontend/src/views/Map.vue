fb5fbcda67ba029bccf1a405d426736ec43806db
Yifan Song <Eric_YF_Song@outlook.com>
2022-05-20 18:35:42 +0800

    Can render map marks

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <img class="mb-3" src="@/assets/GreyBox.svg" alt="Default Grey Box" />
+        <!-- <img class="mb-3" src="@/assets/GreyBox.svg" alt="Default Grey Box" /> -->
         <div ref="map" style="width:100%; height:100%"></div>
     </div>
 </template>
@@ -11,12 +11,12 @@ import Map from "ol/Map";
 import TileLayer from "ol/layer/Tile";
 import OSM from "ol/source/OSM";
 import { fromLonLat } from "ol/proj";
+import "ol/ol.css";
 import Point from "ol/geom/Point";
 import Feature from "ol/Feature";
 import { Vector as VectorLayer } from "ol/layer";
 import { Vector as VectorSource } from "ol/source";
 import { Style, Icon } from "ol/style"
-import "ol/ol.css";
 
 export default {
     name: "Map",
@@ -30,11 +30,20 @@ export default {
 
     methods: {
         createMap() {
+            var map = new Map({
+                target: this.$refs['map'],
+                layers: [new TileLayer({ source: new OSM() })],
+                view: new View({
+                    zoom: 5,
+                    center: fromLonLat([120, 30]),
+                    constrainResolution: true
+                }),
+            });
+            var viewProjection = map.getView().getProjection();
             var feature = new Feature({
-                geometry: new Point([180, 30.03]),
+                geometry: new Point([120, 30.03]).transform('EPSG:4326', viewProjection),
                 name: "A mark"
             });
-            alert(new Point([120, 30]))
             feature.setStyle([
                 new Style({
                     image: new Icon({
@@ -50,16 +59,7 @@ export default {
             var vectorLayer = new VectorLayer({
                 source: v_source
             });
-            var layers = [new TileLayer({ source: new OSM() }), vectorLayer];
-            new Map({
-                target: this.$refs['map'],
-                layers: layers,
-                view: new View({
-                    zoom: 5,
-                    center: fromLonLat([120, 30]),
-                    constrainResolution: true
-                }),
-            });
+            map.addLayer(vectorLayer);
         },
     }
 };
