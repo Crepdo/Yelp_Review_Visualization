235a6479c4b740406c972ce34776311cdd4fcf2e
Yifan Song <Eric_YF_Song@outlook.com>
2022-05-23 17:30:16 +0800

    Added get_star function framework

--- precompute/precompute.py
+++ precompute/precompute.py
@@ -1,14 +1,26 @@
-def example_function(a: int) -> int:
+import pandas as pd
+
+def get_star_per_month(business_id):
     '''
-    An example function.
-    :param a: An integer.
-    :returns: The square of a.
+    Get star of a shop in each month
+    :param business_id: The shop's id
+    :returns: Star of the shop every month as a list, the start month and the end month
     '''
-    return a * a
+    data = pd.read_csv("./data/new_orlean_restaurant_reviews.csv")
+
+    # The followings are examples, plz change their values and return
+    stars = []
+    start_year = 2000
+    start_month = 12
+    end_year = 2000
+    end_month = 12
+    return (stars, start_year, start_month, end_year, end_month)
 
 
+# You can test your function here
 def main():
-    example_function(2)
+    a_sample_business_id_not_exist = 000000
+    print(get_star_per_month(a_sample_business_id_not_exist))
 
 if __name__ == "__main__":
     main()
